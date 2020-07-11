35bad49c8c30aee4898a7588f0e789c6fd874b21
aimonansari06 <aimon_ansari06@outlook.com>
2020-07-11 17:00:46 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function getNum(num)
+{
+
+    var result=document.getElementById("result");
+    result.value+=num;
+
+}
+function clr()
+{
+    var result=document.getElementById("result");
+    result.value ="";
+
+}
+function getresult()
+{
+    var result=document.getElementById("result");
+    result.value=eval(result.value);
+}
